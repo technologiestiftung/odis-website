29c78157985fb12763f5851303e633b8ecef7f22
ff6347 <fabianmoronzirfas@protonmail.ch>
2022-03-07 10:12:03 +0100

    ci(build grundsicherung): build after site

--- build-scripts/buildprocess.sh
+++ build-scripts/buildprocess.sh
@@ -18,20 +18,20 @@ function main() {
   echo "running build"
   npm run build
   echo "creating project folder"
-  mkdir -p "../../source/projekte/${TARGET}"
+  mkdir -p "../../_site/projekte/${TARGET}"
   echo "copying build files to project folder"
-  rsync -avz ./build/* "../../source/projekte/${TARGET}"
+  rsync -avz ./build/* "../../_site/projekte/${TARGET}"
   echo "moving to root"
   cd ../../
   echo "copy content of index.html in project into tmp"
-  cat "source/projekte/${TARGET}/index.html" >tmp/tmp_index.html
+  cat "_site/projekte/${TARGET}/index.html" >tmp/tmp_index.html
   echo "adding yml frontmatter to new index.html"
   cat build-scripts/grundsicherung.yml >tmp/index.html
   echo "adding content of old index.html again to new index.html"
   echo "" >>tmp/index.html
   cat tmp/tmp_index.html >>tmp/index.html
   echo "copy index to destination"
-  cp tmp/index.html "source/projekte/${TARGET}/index.html"
+  cp tmp/index.html "_site/projekte/${TARGET}/index.html"
   echo "removing tmp folder"
   rm -rf ./tmp
   exit 0
