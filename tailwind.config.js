7573e2b2eeda497f6175fe848e4b6946d7ce3854
Lisa-Stubert <lisastubert@web.de>
2023-05-12 13:34:52 +0200

    style: improve aktuelles and style newletter archiv

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     
     colors: {
       red: {
-      "500": "#E60032"
+        "500": "#E60032"
       }, 
 
       blue: { 
@@ -25,54 +25,54 @@ module.exports = {
 
       grey: {
         "500": "#9FA3B2"
-        },
+      },
     
     },
 
     fontFamily: {
-        clanProMedium: [ 
-          'Clan Medium',
-          'ui-sans-serif',
-        ],
+      clanProMedium: [ 
+        "Clan Medium",
+        "ui-sans-serif",
+      ],
 
-        clanProMediumitalic: [ 
-          'Clan Mediumitalic',
-          'ui-sans-serif',
-        ],
+      clanProMediumitalic: [ 
+        "Clan Mediumitalic",
+        "ui-sans-serif",
+      ],
 
-        clanProBold: [ 
-          'Clan Bold',
-          'ui-sans-serif',
-        ],
+      clanProBold: [ 
+        "Clan Bold",
+        "ui-sans-serif",
+      ],
 
-        clanProBolditalic: [ 
-          'Clan Bolditalic',
-          'ui-sans-serif',
-        ],
+      clanProBolditalic: [ 
+        "Clan Bolditalic",
+        "ui-sans-serif",
+      ],
 
-        clanProBook: [ 
-          'Clan Book',
-          'ui-sans-serif',
-        ],
+      clanProBook: [ 
+        "Clan Book",
+        "ui-sans-serif",
+      ],
 
-        clanProBookitalic: [ 
-          'Clan Bookitalic',
-          'ui-sans-serif',
-        ],
+      clanProBookitalic: [ 
+        "Clan Bookitalic",
+        "ui-sans-serif",
+      ],
 
-        clanProNews: [ 
-          'Clan News',
-          'ui-sans-serif',
-        ],
+      clanProNews: [ 
+        "Clan News",
+        "ui-sans-serif",
+      ],
 
-        clanProNewsitalic: [ 
-          'Clan Newsitalic',
-          'ui-sans-serif',
-        ],
-      },
-      
+      clanProNewsitalic: [ 
+        "Clan Newsitalic",
+        "ui-sans-serif",
+      ],
+    },
     
-      extend: {},
+  
+    extend: {},
   },
 
   plugins: [],
