50789e23544503b4467e0e5e2fa0e286f071ebed
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-08-25 14:17:43 +0200

    fix: include fonts cleanly

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,73 +2,46 @@
 module.exports = {
   content: ["./**/*.{liquid,html}"],
   theme: {
-    
     colors: {
       red: {
-        "500": "#E60032"
-      }, 
-
-      blue: { 
-        "100": "#91A6E8",
-        "500": "#1E398F",
-        "900": "#0C163B" 
+        500: "#E60032",
       },
 
-      cyan: { 
-        "50": "#FDFEFF",
-        "100": "#EDF8FE",
+      blue: {
+        100: "#91A6E8",
+        500: "#1E398F",
+        900: "#0C163B",
+      },
+
+      cyan: {
+        50: "#FDFEFF",
+        100: "#EDF8FE",
       },
 
       grey: {
-        "500": "#9FA3B2"
+        500: "#9FA3B2",
       },
-    
     },
-
-    fontFamily: {
-      clanProMedium: [ 
-        "Clan Medium",
-        "ui-sans-serif",
-      ],
-
-      clanProMediumitalic: [ 
-        "Clan Mediumitalic",
-        "ui-sans-serif",
-      ],
-
-      clanProBold: [ 
-        "Clan Bold",
-        "ui-sans-serif",
-      ],
-
-      clanProBolditalic: [ 
-        "Clan Bolditalic",
-        "ui-sans-serif",
-      ],
-
-      clanProBook: [ 
-        "Clan Book",
-        "ui-sans-serif",
-      ],
-
-      clanProBookitalic: [ 
-        "Clan Bookitalic",
-        "ui-sans-serif",
-      ],
-
-      clanProNews: [ 
-        "Clan News",
-        "ui-sans-serif",
-      ],
-
-      clanProNewsitalic: [ 
-        "Clan Newsitalic",
-        "ui-sans-serif",
-      ],
+    extend: {
+      fontFamily: {
+        sans: [
+          "Clan",
+          "ui-sans-serif",
+          "Calibri",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
     },
-    
-  
-    extend: {},
   },
 
   plugins: [],
