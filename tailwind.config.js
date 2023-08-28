769aa67bc2c627d83684953bcf2934605da27617
Lucas Vogel <contact@vogelino.com>
2023-08-28 12:15:37 +0200

    refactor(tailwind): Refactor colors to use figma structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,62 @@ module.exports = {
   content: ["./**/*.{liquid,html}"],
   theme: {
     colors: {
-      red: {
-        DEFAULT: "#E60032",
-        dark: "#C42C3A",
+      // USE THESE SEMANIC COLORS
+      bg: {
+        DEFAULT: "var(--bg)",
+        alt: "var(--bg-alt)",
+        inv: "var(--bg-inv)",
       },
-      blue: {
-        ultralight: "#EDF8FE",
-        light: "#BEE4F8",
-        medium: "#4C64CD",
-        DEFAULT: "#20378B",
-        dark: "#172763",
-        darker: "#0C163B",
+      text: {
+        DEFAULT: "var(--text)",
+        inv: "var(--text-inv)",
       },
-      grey: {
-        light: "#D1DBE0",
+      links: {
+        DEFAULT: "var(--links)",
+        inv: "var(--links-inv)",
+        active: "var(--links-active)",
+        "active-inv": "var(--links-active-inv)",
       },
-      white: "#FFFFFF",
+      headlines: {
+        DEFAULT: "var(--headlines)",
+        inv: "var(--headlines-inv)",
+      },
+      line: {
+        DEFAULT: "var(--line)",
+        dark: "var(--line-dark)",
+        inv: "var(--line-inv)",
+        "dark-inv": "var(--line-dark-inv)",
+      },
+      shadow: {
+        primary: {
+          DEFAULT: "var(--shadow-primary)",
+          inv: "var(--shadow-primary-inv)",
+        },
+        secondary: {
+          DEFAULT: "var(--shadow-secondary)",
+          inv: "var(--shadow-secondary-inv)",
+        },
+      }
+
+      // AVOID USING PRIMITIVE COLORS DIRECTLY, RATHER USE SEMANTIC COLORS (SEE ABOVE)
+      // If you still need to, use the css variable directly (eg. var(--red))
+      // ...or uncomment the following lines to use with tailwind (avoid this if possible)
+      // red: {
+      //   DEFAULT: "var(--red)",
+      //   dark: "var(--red-dark)",
+      // },
+      // blue: {
+      //   ultralight: "var(--blue-ultralight)",
+      //   light: "var(--blue-light)",
+      //   medium: "var(--blue-medium)",
+      //   DEFAULT: "var(--blue)",
+      //   dark: "var(--blue-dark)",
+      //   darker: "var(--blue-darker)",
+      // },
+      // grey: {
+      //   light: "var(--grey-light)",
+      // },
+      // white: "var(--white)",
     },
     fontFamily: {
       sans: [
