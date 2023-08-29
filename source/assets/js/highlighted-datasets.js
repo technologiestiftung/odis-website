c55864110c01e6902a3ac8d476c5882bb8e44cec
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-08-29 17:51:39 +0200

    feat: optimize slider for medium-sized screens

--- source/assets/js/highlighted-datasets.js
+++ source/assets/js/highlighted-datasets.js
@@ -10,10 +10,20 @@ SLIDER (mobile screen sizes)
 const INITIAL_SLIDE_INDEX = 1;
 
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 1.4, // 2.2 or similar on tablet size
+  slidesPerView: 1.4,
+  spaceBetween: 24,
+  breakpoints: {
+    480: {
+      slidesPerView: 1.8,
+      spaceBetween: 40
+    },
+    640: {
+      slidesPerView: 2.2,
+      spaceBetween: 48
+    }
+  },
   initialSlide: INITIAL_SLIDE_INDEX,
   centeredSlides: true,
-  spaceBetween: 24,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -24,17 +34,37 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-[...highlightedDatasetsList][INITIAL_SLIDE_INDEX].classList.remove("hidden");
+const initialInfoElement = [...highlightedDatasetsList][INITIAL_SLIDE_INDEX]
 
-swiper.on("transitionEnd", function () {
+initialInfoElement.classList.remove("hidden");
+alignHorizontally(initialInfoElement, swiper.slides[INITIAL_SLIDE_INDEX])
+
+swiper.on("transitionEnd", function (swiper) {
   [...highlightedDatasetsList].forEach((element) => {
     const isActive =
       element.dataset.highlightedDatasetIndex ===
       `${Number(swiper.activeIndex)}`;
 
     element.classList.toggle("hidden", !isActive);
+
+    if (isActive) {
+      const activeSlide = swiper.slides[swiper.activeIndex]
+      alignHorizontally(element, activeSlide)
+    }
   });
 });
+
+/**
+ * Aligns two elements horizontally by taking the positioning of the referenceElement and applying its left offset as a horizontal padding to the targetElement.
+ * @param {HTMLElement} targetElement 
+ * @param {HTMLElement} referenceElement 
+ */
+function alignHorizontally(targetElement, referenceElement) {
+  const referenceOffset = referenceElement.getBoundingClientRect().left
+
+  targetElement.style.paddingLeft = `${referenceOffset}px`
+  targetElement.style.paddingRight = `${referenceOffset}px`
+}
 
 /*
 ------------------------------------------------------------
