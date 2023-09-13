e5f6a9154d74de62633dd004f4bc23ff45e8d249
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2023-09-13 16:07:15 +0200

    feat: improve TOC for news article

--- source/assets/js/tableOfContents.js
+++ source/assets/js/tableOfContents.js
@@ -4,11 +4,15 @@ function createTableOfContents(containerId, targetId) {
   const toc = document.createElement("nav");
   const list = document.createElement("ul");
   toc.setAttribute("id", "toc");
-  toc.setAttribute("aria-label", "Table of Contents");
-  list.setAttribute("class", "toc-list");
+  toc.setAttribute("aria-labelledby", "toc-label");
+  list.setAttribute("class", "toc-list mt-5 grid gap-3");
   toc.appendChild(list);
   headings.forEach((heading) => {
     const listItem = document.createElement("li");
+    listItem.setAttribute(
+      "class",
+      "transition-colors text-links hover-hover:hover:text-links-active",
+    );
     const link = document.createElement("a");
     const text = document.createTextNode(heading.textContent);
     const slug = heading.textContent
@@ -27,6 +31,21 @@ function createTableOfContents(containerId, targetId) {
 
     link.setAttribute("href", "#" + slug);
     link.appendChild(text);
+
+    /**
+     * Calculates a horizontal offset based on the heading level that can be used for indenting elements.
+     * @param {number} n
+     * @returns number
+     */
+    const getOffsetByLevel = (n) => (n == 2 ? 0 : Math.ceil(Math.exp(n)));
+
+    const headingLevel = parseInt(heading.tagName.replace(/h/i, ""), 10);
+
+    listItem.style.transform = `translateX(${getOffsetByLevel(
+      headingLevel,
+    )}px)`;
+    listItem.classList.add("leading-tight");
+
     listItem.appendChild(link);
     list.appendChild(listItem);
   });
