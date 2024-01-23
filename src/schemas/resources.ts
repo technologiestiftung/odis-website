6df5fc9703b825759daa6c122a62393a46c196b0
Lucas Vogel <contact@vogelino.com>
2024-01-23 19:01:16 -0300

    feat(ResourceCard): Basic scaffold for ResourceCard WIP

--- src/schemas/resources.ts
+++ src/schemas/resources.ts
@@ -4,12 +4,16 @@ import { collectionSchema, pageSchema } from "./common";
 export const resourcesSchema = ({ image }: { image: ImageFunction }) =>
   collectionSchema({ image }).merge(
     z.object({
-      data: z.coerce.date().default(new Date()),
-      tags: z.array(z.string()).default(["news"]),
-      urlText: z.string().default("Zum Blogbeitrag"),
-      author: z.string().optional(),
-      readingTime: z.union([z.string(), z.number()]).optional(),
-      link: z.string().optional(),
+      date: z.date().default(new Date()),
+      tags: z.array(z.string()).default(["resource"]),
+      question: z.string().optional().nullable(),
+      media: z.object({
+        thumbnail: image(),
+        type: z.enum(["video", "pdf", "blog", "table", "project", "other"]).default("other"),
+        readingOrPlayingTime: z.string().optional().nullable(),
+        size: z.string().optional().nullable(),
+        link: z.string().optional().nullable(),
+      })
     }),
   );
 
