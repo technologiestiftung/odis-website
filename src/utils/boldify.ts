000d032f795c16c016ed8d4cbe07290ab765ab46
Lucas Vogel <contact@vogelino.com>
2024-01-30 15:22:05 -0300

    feat(homepage): Add HomepageProjectsSection

--- src/utils/boldify.ts
+++ src/utils/boldify.ts
@@ -1,5 +1,8 @@
-export const boldify = (str: string): string => {
+export const boldify = (str: string, className = ""): string => {
   const includesBr = str.includes("<br />") || str.includes("<br/>");
   if (!includesBr) return `<strong>${str}</strong>`;
-  return str.replaceAll(/\*\*(.+)\*\*/g, "<strong>$1</strong>");
+  return str.replaceAll(
+    /\*\*(.+)\*\*/g,
+    `<strong class='${className} text-balance block'>$1</strong>`,
+  );
 };
