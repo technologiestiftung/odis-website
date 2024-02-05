317a1cf8246c9feb26e9ca368249add5260a405c
Lucas Vogel <contact@vogelino.com>
2024-02-05 16:21:10 -0300

    ci(netlify): Attempt to deploy with netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,23 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.PROD
-    ? import.meta.env.VERCEL_URL || "https://odis-berlin.de"
-    : `http://localhost:${process.env.PORT || 4321}`,
-  integrations: [
-    icon({ iconDir: "src/assets/images/icons" }),
-    tailwind(),
-    mdx(),
-    prefetch(),
-    sitemap(),
-  ],
+  site: import.meta.env.PROD ? import.meta.env.VERCEL_URL || "https://odis-berlin.de" : `http://localhost:${process.env.PORT || 4321}`,
+  integrations: [icon({
+    iconDir: "src/assets/images/icons"
+  }), tailwind(), mdx(), prefetch(), sitemap()],
   redirects: {
     "/projekte/organigramme/guide": "/aktuelles/2022-02-18-organigramm-guide",
     "/projekte/xml-validator/tool": "/xml-validator-tool",
-    "/projekte/organigramme/guide": "/aktuelles/2022-02-18-organigramm-guide",
+    "/projekte/organigramme/guide": "/aktuelles/2022-02-18-organigramm-guide"
   },
+  output: "server",
+  adapter: netlify()
 });
