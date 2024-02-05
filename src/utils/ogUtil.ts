5a9d6ac9051a8cc86a6df7c08d727af4ded64422
Lucas Vogel <contact@vogelino.com>
2024-02-05 15:39:58 -0300

    fix(og-images): Disable tsb logo to intent to deploy successfully

--- src/utils/ogUtil.ts
+++ src/utils/ogUtil.ts
@@ -63,9 +63,9 @@ export async function renderBasicOgImage({
   };
   logoPath?: string;
 }) {
-  const logoData = await fetch(new URL(import.meta.env.SITE + logoPath)).then(
-    (res) => res.arrayBuffer(),
-  );
+  // const logoData = await fetch(new URL(import.meta.env.SITE + logoPath)).then(
+  //   (res) => res.arrayBuffer(),
+  // );
   // let imageData;
   // if (image?.src) {
   //   console.log("fetching image", import.meta.env.SITE + image.src);
@@ -89,13 +89,13 @@ export async function renderBasicOgImage({
           tw: cn("w-1/2 flex flex-col justify-center px-16 pt-12 pb-16"),
         },
         [
-          el("img", {
-            src: logoData,
-            tw: cn("mb-4", styleOverrides.logo),
-            width: 300,
-            height: 72,
-            key: "logo",
-          }),
+          // el("img", {
+          //   src: logoData,
+          //   tw: cn("mb-4", styleOverrides.logo),
+          //   width: 300,
+          //   height: 72,
+          //   key: "logo",
+          // }),
           prefix &&
             el(
               "p",
