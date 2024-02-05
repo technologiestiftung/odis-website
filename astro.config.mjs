d757c8d7c3c279aaaa5295438127455f28da85c5
Lucas Vogel <contact@vogelino.com>
2024-02-05 16:22:21 -0300

    ci(netlify): Enable page caching

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,15 +9,23 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.PROD ? import.meta.env.VERCEL_URL || "https://odis-berlin.de" : `http://localhost:${process.env.PORT || 4321}`,
-  integrations: [icon({
-    iconDir: "src/assets/images/icons"
-  }), tailwind(), mdx(), prefetch(), sitemap()],
+  site: import.meta.env.PROD
+    ? import.meta.env.VERCEL_URL || "https://odis-berlin.de"
+    : `http://localhost:${process.env.PORT || 4321}`,
+  integrations: [
+    icon({
+      iconDir: "src/assets/images/icons",
+    }),
+    tailwind(),
+    mdx(),
+    prefetch(),
+    sitemap(),
+  ],
   redirects: {
     "/projekte/organigramme/guide": "/aktuelles/2022-02-18-organigramm-guide",
     "/projekte/xml-validator/tool": "/xml-validator-tool",
-    "/projekte/organigramme/guide": "/aktuelles/2022-02-18-organigramm-guide"
+    "/projekte/organigramme/guide": "/aktuelles/2022-02-18-organigramm-guide",
   },
   output: "server",
-  adapter: netlify()
+  adapter: netlify({ cacheOnDemandPages }),
 });
