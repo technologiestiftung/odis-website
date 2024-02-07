b8acf547da041d060b9e7f01a18666649b758ae0
Lucas Vogel <contact@vogelino.com>
2024-02-07 10:30:57 -0300

    ci(purgecss): Add Astro purgecss to remove unused classes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // Comunity plugins
 import icon from "astro-icon";
-
+import purgecss from "astro-purgecss";
 const hasMatomo = process.env.MATOMO_URL && process.env.MATOMO_SITE_ID;
 if (!hasMatomo) {
   console.warn(
@@ -30,6 +30,15 @@ export default defineConfig({
     mdx(),
     prefetch(),
     sitemap(),
+    purgecss({
+      extractors: [
+        {
+          extractor: (content) =>
+            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+          extensions: ["astro", "html"],
+        },
+      ],
+    }),
   ],
   redirects: {
     "/projekte/organigramme/guide": "/aktuelles/2022-02-18-organigramm-guide",
